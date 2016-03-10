053261238b04730c4c5e64ac72d971125d62d61c
waitingForY <1154043153@qq.com>
2016-03-10 18:15:21 +0800

    add pushbar chart and change all charts can reuse the same style

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,13 @@
-    var gridster;
+
+//main.js
+var gridster;
     $(function(){
         gridster = $(".gridster ul").gridster({    //通过jquery选择DOM实现gridster
             widget_base_dimensions: [100, 120],    //模块的宽高 [宽,高]
             widget_margins: [5, 5],    //模块的间距 [上下,左右]
+//            draggable: {
+//                handle: '.con'    //模块内定义拖动的元素<header>，这里也支持jquery选择器，如"span.drag_handle"
+//            }
             draggable:{
                 start: function(event, ui){
 
@@ -23,14 +28,9 @@
         }).data('gridster');
     });
     $("#save").click(function () {
-        var sty={
-            color:'#ffffff',
-            fontSize:'15'
-        };
-        var title={text:document.getElementById("title").value,style:sty};
-        //var xAxis=document.getElementById('xAxis').value;
-        var xAxis={title:{text:document.getElementById('xAxis').value,style:sty},labels:{style:sty}};
-        var yAxis={title:{text:document.getElementById('yAxis').value,style:sty},labels:{style:sty}};
+        var title_text=document.getElementById("title").value;
+        var xAxis_text=document.getElementById('xAxis').value;
+        var yAxis_text=document.getElementById('yAxis').value;
         var channel=document.getElementById('msgid').value;
         var li=gridster.add_widget('<li class="con"></li>', 4, 2);
         var div=$('<div></div>', {
@@ -38,24 +38,20 @@
             margin:10+"px",
             width:"100%",
             height:"100%",
-            background:"#adadad"
+            background:"#ffffff"
         }).appendTo($(li));
         var socket=io.connect('http://localhost:3000');
 
-        var msg={};
-        msg.channel=channel;
-        socket.emit('chartmsg',msg);
-        socket.on('pushData',function (data) {
-             var payload=data;
-             payload.title=title;
-             if(payload.xAxis){
-                 payload.xAxis=xAxis;
-             }
-             if(payload.yAxis)
-             {
-                  payload.yAxis=yAxis;
-             }
-             $(div).highcharts(payload);
-
-        });
+    var msg={};
+    msg.channel=channel;
+    socket.emit('chartmsg',msg);
+    socket.on('pushData',function (data) {
+         var payload=data;
+         payload.title.text=title_text;
+        if(payload.yAxis)
+        {
+            payload.yAxis.title.text=yAxis_text;
+        }
+        $(div).highcharts(payload);
     });
+});
--- redisReader/testPublish.js
+++ redisReader/testPublish.js
@@ -35,75 +35,22 @@ client.on('error',function(error){
 // });
 //publish.js
 
-var NewYork={
+var pushline={
 	name:'New York',
 	data:[-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8,24.1, 20.1, 14.1, 8.6, 2.5],
 	categories:['一月', '二月', '三月', '四月', '五月', '六月' ,'七月', '八月', '九月', '十月', '十一月', '十二月']
 	};
-var Berlin={
-            name: 'Berlin',
-            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6,17.9, 14.3, 9.0, 3.9, 1.0],
-            categories:['一月', '二月', '三月', '四月', '五月', '六月' ,'七月', '八月', '九月', '十月', '十一月', '十二月']
-        };
-var London={
-            name: 'London',
-            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0,16.6, 14.2, 10.3, 6.6, 4.8],
-            categories:['一月', '二月', '三月', '四月', '五月', '六月' ,'七月', '八月', '九月', '十月', '十一月', '十二月']
-        };
  var pushcolum={
- 	name: 'Population',
-             data: [
-                ['Shanghai', 23.7],
-                ['Lagos', 16.1],
-                ['Instanbul', 14.2],
-                ['Karachi', 14.0],
-                ['Mumbai', 12.5],
-                ['Moscow', 12.1],
-                ['São Paulo', 11.8],
-                ['Beijing', 11.7],
-                ['Guangzhou', 11.1],
-                ['Delhi', 11.1],
-                ['Shenzhen', 10.5],
-                ['Seoul', 10.4]
-            ]
+    categories: [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ],
+    name: 'Population',
+    data:[49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
  };
 var pusharea={
+        categories: [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday' ],
         name: 'USA',
-        data: [null, null, null, null, null, 6, 11, 32, 110, 235, 369, 640,
-            1005, 1436, 2063, 3057, 4618, 6444, 9822, 15468, 20434, 24126,
-            27387, 29459, 31056, 31982, 32040, 31233, 29224, 27342, 26662,
-            26956, 27912, 28999, 28965, 27826, 25579, 25722, 24826, 24605,
-            24304, 23464, 23708, 24099, 24357, 24237, 24401, 24344, 23586,
-            22380, 21004, 17287, 14747, 13076, 12555, 12144, 11009, 10950,
-            10871, 10824, 10577, 10527, 10475, 10421, 10358, 10295, 10104]
-};
-var pushhash={
-        name: 'Brands',
-        colorByPoint: true,
-        data: [{
-            name: 'Microsoft Internet Explorer',
-            y: 56.33
-        }, {
-            name: 'Chrome',
-            y: 24.03,
-            sliced: true,
-            selected: true
-        }, {
-            name: 'Firefox',
-            y: 10.38
-        }, {
-            name: 'Safari',
-            y: 4.77
-        }, {
-            name: 'Opera',
-            y: 0.91
-        }, {
-            name: 'Proprietary or Undetectable',
-            y: 0.2
-        }]
+        data: [3, 4, 3, 5, 4, 10, 12]
 };
 var pushpie={
-    // type: 'pie',
     name: 'Browser share',
     data: [
         ['Firefox', 45.0],
@@ -119,19 +66,23 @@ var pushpie={
         ['Others', 0.7]
     ]
 };
+var pushbar={
+    categories: ['Africa', 'America', 'Asia', 'Europe', 'Oceania'],
+    series: [{ name: 'Year 1800', data: [107, 31, 635, 203, 2] }, { name: 'Year 1900', data: [133, 156, 947, 408, 6] }, { name: 'Year 2008', data: [973, 914, 4054, 732, 34] }]
+}
+
 
 client.on('ready',function  (error) {
 	if (error) {
 		console.log(error);
 	}else{
 		var interval=setInterval(function  () {
-			client.publish('pushline_NewYork',JSON.stringify(NewYork));
-			client.publish('pushline_Berlin',JSON.stringify(Berlin));
-			client.publish('pushline_London',JSON.stringify(London));
+			client.publish('pushline_test',JSON.stringify(pushline));
 			client.publish('pushcolumn_test',JSON.stringify(pushcolum));
 			client.publish('pusharea_test',JSON.stringify(pusharea));
-			client.publish('pushhash_test',JSON.stringify(pushhash));
 			client.publish('pushpie_test',JSON.stringify(pushpie));
+                                       client.publish('pushbar_test',JSON.stringify(pushbar));
+
 		},2000);
 //		setTimeout(function  () {
 //			clearInterval(interval);
