a12e1f5526d7a5ab6a66c5c6f2ad10a20f4b19cc
lqllyf <78543900@qq.com>
2016-03-10 16:26:24 +0800

    i have make the style same

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,13 +1,8 @@
-
-//main.js
-var gridster;
+    var gridster;
     $(function(){
         gridster = $(".gridster ul").gridster({    //通过jquery选择DOM实现gridster
             widget_base_dimensions: [100, 120],    //模块的宽高 [宽,高]
             widget_margins: [5, 5],    //模块的间距 [上下,左右]
-//            draggable: {
-//                handle: '.con'    //模块内定义拖动的元素<header>，这里也支持jquery选择器，如"span.drag_handle"
-//            }
             draggable:{
                 start: function(event, ui){
 
@@ -33,8 +28,9 @@ var gridster;
             fontSize:'15'
         };
         var title={text:document.getElementById("title").value,style:sty};
-        var xAxis=document.getElementById('xAxis').value;
-        var yAxis={text:document.getElementById('yAxis').value};
+        //var xAxis=document.getElementById('xAxis').value;
+        var xAxis={title:{text:document.getElementById('xAxis').value,style:sty},labels:{style:sty}};
+        var yAxis={title:{text:document.getElementById('yAxis').value,style:sty},labels:{style:sty}};
         var channel=document.getElementById('msgid').value;
         var li=gridster.add_widget('<li class="con"></li>', 4, 2);
         var div=$('<div></div>', {
@@ -46,24 +42,20 @@ var gridster;
         }).appendTo($(li));
         var socket=io.connect('http://localhost:3000');
 
-    var msg={};
-    //msg.title=title;
-    //msg.yAxis=yAxis;
-    msg.channel=channel;
-    //msg.xAxis=xAxis;
-    socket.emit('chartmsg',msg);
-    socket.on('pushData',function (data) {
-        //console.log(JSON.stringify(data));
-         var payload=data;
-        //payload.className='chart';
-         payload.title=title;
-        if(payload.yAxis)
-        {
-            payload.yAxis.title=yAxis;
-        }
-        //payload.className='test';
-        //var chart=li.highcharts(payload);
-        $(div).highcharts(payload);
+        var msg={};
+        msg.channel=channel;
+        socket.emit('chartmsg',msg);
+        socket.on('pushData',function (data) {
+             var payload=data;
+             payload.title=title;
+             if(payload.xAxis){
+                 payload.xAxis=xAxis;
+             }
+             if(payload.yAxis)
+             {
+                  payload.yAxis=yAxis;
+             }
+             $(div).highcharts(payload);
 
+        });
     });
-});
